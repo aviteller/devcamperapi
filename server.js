f498f3feb97670f0bfa5a056b307c054fc25f7c7
unknown <aviteller2@gmail.com>
2020-03-31 15:16:55 +0100

    mit

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// const logger = require('./middleware/logger')
+const morgan = require("morgan");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+
+// route files
+const bootcamps = require("./routes/bootcamps");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// connect to db
+connectDB();
+
 const app = express();
+//body parser
+app.use(express.json());
+// Dev Logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//mount router
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// handle unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //close server & exit proccess
+  server.close(() => process.exit(1));
+});
